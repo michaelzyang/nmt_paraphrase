b5f898867cca8c1cf6949b2cf7d9c64600a06977
Rahul <rmayuran@andrew.cmu.edu>
2020-03-12 00:17:41 -0400

    Refactor in progress

--- data_processing.py
+++ data_processing.py
@@ -12,16 +12,16 @@ def json_to_dict(filename):
         return json.load(infile)
 
 
-def read_dataset(filename, source_dictionary, target_dictionary):
-    with open(filename, "r") as infile:
-        for line in infile:
-            src = [SOS_TOKEN] + [source_dictionary.get(x, UNK) for x in line.strip().split(" ")] + [EOS_TOKEN]
-            tgt = [SOS_TOKEN] + [target_dictionary.get(x, UNK) for x in line.strip().split(" ")] + [EOS_TOKEN]
+def read_dataset(source_filename, target_filename, source_dictionary, target_dictionary):
+    with open(source_filename, "r") as sfile, open(target_filename) as tfile:
+        for src_line, tgt_line in zip(sfile, tfile):
+            src = [SOS_TOKEN] + [source_dictionary.get(x, UNK) for x in src_line.strip().split(" ")] + [EOS_TOKEN]
+            tgt = [SOS_TOKEN] + [target_dictionary.get(x, UNK) for x in tgt_line.strip().split(" ")] + [EOS_TOKEN]
             yield src, tgt
 
 
 class NMTData(Dataset):
-    def __init__(self, TRAIN, SRC_DICT, TGT_DICT):
+    def __init__(self, SOURCE, TARGET, SRC_DICT, TGT_DICT):
         self.source = list()
         self.target = list()
         self.source_mask = list()
@@ -33,23 +33,28 @@ class NMTData(Dataset):
         source_dict = json_to_dict(SRC_DICT)
         target_dict = json_to_dict(TGT_DICT)
 
-        for item in read_dataset(TRAIN, source_dict, target_dict):
+        for item in read_dataset(SOURCE, TARGET, source_dict, target_dict):
             self.maxlen_source = max(self.maxlen_source, len(item[0]))
             self.maxlen_target = max(self.maxlen_target, len(item[1]))
             self.source.append(item[0])
             self.target.append(item[1])
+            self.length += 1
 
-        for i in range(self.source):
+        for i in range(len(self.source)):
             diff = self.maxlen_source - len(self.source[i])
-            self.source_mask[i] += [False] * len(self.source[i])
+            mask = list()
+            mask += [False] * len(self.source[i])
             self.source[i] += [EOS_TOKEN] * diff
-            self.source_mask[i] += [True] * diff
+            mask += [True] * diff
+            self.source_mask.append(mask)
 
-        for i in range(self.target):
+        for i in range(len(self.target)):
             diff = self.maxlen_target - len(self.target[i])
-            self.target_mask[i] += [False] * len(self.target[i])
+            mask = list()
+            mask += [False] * len(self.target[i])
             self.target[i] += [EOS_TOKEN] * diff
-            self.target_mask[i] += [True] * diff
+            mask += [True] * diff
+            self.target_mask.append(mask)
 
         self.source = torch.from_numpy(np.asarray(self.source))
         self.target = torch.from_numpy(np.asarray(self.target))
@@ -60,7 +65,7 @@ class NMTData(Dataset):
         return self.length
 
     def __getitem__(self, index):
-        return self.train_source[index], self.train_target[index], self.source_mask[index], self.target_mask[index]
+        return self.source[index], self.source_mask[index], self.target[index], self.target_mask[index]
 
 
 def idxs_to_sentences(tgt_list, tgt_vocab):
